235580ac841d2db4ca77767574a9d3d7499cbf52
malmayahi <mzalmayahi@gmail.com>
2024-08-07 17:55:49 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dynamic Website</title>
+</head>
+<body>
+    <h1>Welcome to the Dynamic Website of Muhammed Al Mayahi-Ellucian</h1>
+    <p>This is a dynamic website hosted on AWS EC2 for testing and learning purpose.</p>
+    <p>Current Server Time: <?php echo date('Y-m-d H:i:s'); ?></p>
+</body>
+</html>
